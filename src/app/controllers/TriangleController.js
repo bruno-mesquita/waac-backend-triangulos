6bc9263b01763d3543b9c4ce3056df65e41109ae
Bruno Silva <bmesquita196@gmail.com>
2019-11-13 19:06:00 -0300

    add yup e validando entrada do array

--- src/app/controllers/TriangleController.js
+++ src/app/controllers/TriangleController.js
@@ -1,7 +1,17 @@
+import * as Yup from 'yup';
 import Triangle from '../models/Triangle';
 
 class TriangleController {
   async store(req, res) {
+    // Schema de Validação
+    const schema = Yup.object().shape({
+      content: Yup.array(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(401).json({ error: 'Falha na Validação' });
+    }
+
     const { content, id } = await Triangle.create(req.body);
 
     return res.json({
